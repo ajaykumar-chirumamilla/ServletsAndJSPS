64a0da9a0ce90515bcca8ba370c0311b8b44335f
Ajay Kumar Chirumamilla <ajaykumar2789@gmail.com>
2015-09-28 12:39:40 -0700

    Servlets And Jsps
    Uploaded Servlets and JSPS programs

---
+++ ServletAndJSP_WorkSpace/MVC/src/beans/User.java
@@ -0,0 +1,23 @@
+package beans;
+
+public class User {
+	private String email;
+	private String password;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String emailAddr) {
+		this.email = emailAddr;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
---
+++ ServletAndJSP_WorkSpace/Scriptlets/src/gui/TextOutput.java
@@ -0,0 +1,8 @@
+package gui;
+
+public class TextOutput {
+public String getInfo()
+{
+	return "This is a method returned from class with in the gui package";
+}
+}
